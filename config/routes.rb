756b552e7621fb2e16c6de7832f53537dc2254f7
Chelsie Conrad <chelsiechey@gmail.com>
2021-04-02 15:59:05 -0600

    styled the user sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
     get 'login', to: 'devise/sessions#new'
   end
 
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+  end
+
   root to: 'pages#index'
 end
